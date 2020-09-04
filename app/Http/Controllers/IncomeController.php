f372d94501a8052d96fe6fcc4fa278debdfbfd7d
William <william.odiomonafe@gmail.com>
2020-09-05 00:10:33 +0100

    Incomplete Income module

---
+++ app/Http/Controllers/IncomeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Income;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class IncomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+           'amount' => 'required',
+            'date_logged' => 'date',
+        ]);
+
+        $saved_income = Income::create([
+            'title' => $request->title,
+            'amount' => $request->amount,
+            'user_id' => auth()->user()->id,
+        ]);
+        if($saved_income)
+            return response()->json(["message" => "Income Saved"]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Income $income)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Income $income)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_08_30_011504_create_incomes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->decimal('amount');
+            $table->dateTime('date_logged')->useCurrent();
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
