9a332099271551ef1f1fdd16a4905a68f736dab5
wocodes <william.odiomonafe@gmail.com>
2020-11-08 12:39:49 +0100

    Reverted branch to woking version

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         // try to auth and get the token using api authentication
         if (!$token = auth('api')->attempt($credentials)) {
             // if the credentials are wrong we send an unauthorized error in json format
-            return response()->json(['message' => 'Unauthorized', 'status' => 'error'], 401);
+            return response()->json(['message' => 'Invalid Credentials. Try again', 'status' => 'error'], 401);
         }
 
         return response()->json([
